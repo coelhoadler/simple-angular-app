38a2894b39faf1534e9778ce6d54d60e1306fa0b
Adler Coelho <adler.coelho.santos@icloud.com>
2020-08-22 13:33:57 -0300

    Forgot files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-app';
+  username = 'coelhoadler';
+
+  constructor(
+    public appService: AppService
+  ) {
+    this.appService.username = this.username;
+   }
+
+   ngOnInit() {
+    this.appService.username = this.username;
+   }
+
+  callAPI() {
+    this.appService.getGitProjects();
+  }
+
 }
